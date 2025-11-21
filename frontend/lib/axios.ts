4b67cbc70a80aebb70d7af67591f27bc4e2f8d1b
Jose Roque <joseroque@msfissa.com>
2025-11-21 14:50:05 -0400

    2.0

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/Api', 
+  baseURL: 'https://roquesoft.com:5000/Api', 
   headers: {
     'Content-Type': 'application/json',
   },
